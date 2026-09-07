c38986ccad035ce3859ddb715f53a5f2aa32f7a5
Richard Le <169632088+rlechuong@users.noreply.github.com>
2026-09-06 22:36:44 -0400

    feat: add scene response types

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,1 +1,23 @@
-export {}
+export type CharacterOption = {
+  id: number;
+  name: string;
+  thumbnailPublicId: string;
+};
+
+export type SceneSummary = {
+  id: number;
+  name: string;
+  slug: string;
+  publicId: string;
+  characterCount: number;
+};
+
+export type SceneDetail = {
+  id: number;
+  name: string;
+  slug: string;
+  publicId: string;
+  width: number;
+  height: number;
+  characters: CharacterOption[];
+};
